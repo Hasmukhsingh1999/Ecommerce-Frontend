ad5158ab98dfcb0a4212e953a83f77b9ddd9b5e5
Hasmukhsingh1999 <hasmukhsingh90@gmail.com>
2024-01-07 12:47:06 +0530

    ...

--- components/Sale/Sale.jsx
+++ components/Sale/Sale.jsx
@@ -1,48 +1,11 @@
-"use client";
-import React, { useState, useEffect, useRef } from "react";
-import gsap from "gsap";
+import React from 'react'
 
 const Sale = () => {
-  const items = ["ONE", "TWO", "THREE"];
-  const [currentItem, setCurrentItem] = useState(0);
-  const containerRef = useRef(null);
-
-  useEffect(() => {
-    const container = containerRef.current;
-
-    const animateSlide = (index) => {
-      gsap.to(container.children, {
-        x: -index * container.clientWidth,
-        duration: 0.5,
-        ease: "power2.inOut",
-      });
-    };
-
-    const intervalId = setInterval(() => {
-      setCurrentItem((prevItem) => (prevItem + 1) % items.length);
-    }, 2000);
-
-    animateSlide(currentItem);
-
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [items.length, currentItem]);
-
   return (
-    <div className="bg-black h-[3rem] container overflow-hidden flex items-center justify-center">
-      <div ref={containerRef} className="flex">
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className={`w-full bg-red-500 text-center`}
-          >
-            {item}
-          </div>
-        ))}
-      </div>
+    <div className='bg-gray-400 h-[3rem] text-white container flex items-center justify-center'>
+      <div className='text-center m-auto bg-red-500 w-full'>hi</div>
     </div>
-  );
-};
+  )
+}
 
-export default Sale;
+export default Sale
